4007127535e705eeaccac2c704fb7e6befc82ebe
jackcodings <70752581+jackcodings@users.noreply.github.com>
2026-01-06 20:43:53 +0000

    added key press functionality

--- index.js
+++ index.js
@@ -49,3 +49,42 @@ document.querySelectorAll(".drum")[i].addEventListener("click", function () {
 });
 
 }
+
+document.addEventListener("keydown", function (event) {
+  playSound(event.key.toLowerCase());
+
+function playSound(key) {
+  switch (key) {
+    case "w":
+      new Audio("sounds/tom-1.mp3").play();
+      break;
+
+    case "a":
+      new Audio("sounds/tom-2.mp3").play();
+      break;
+
+    case "s":
+      new Audio("sounds/tom-3.mp3").play();
+      break;
+
+    case "d":
+      new Audio("sounds/tom-4.mp3").play();
+      break;
+
+    case "j":
+      new Audio("sounds/snare.mp3").play();
+      break;
+
+    case "k":
+      new Audio("sounds/crash.mp3").play();
+      break;
+
+    case "l":
+      new Audio("sounds/kick-bass.mp3").play();
+      break;
+
+    default:
+      return;
+  }
+}
+});
